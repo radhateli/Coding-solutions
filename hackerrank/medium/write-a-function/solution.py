f6eef797acbf3ff543a741518cc9192e70cb556a
Radha Teli <radha952121@gmail.com>
2026-07-26 11:29:41 +0530

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-26

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,7 +1,15 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a+b)
-    print(a-b)
-    print(a*b)
+def is_leap(year):
+    leap = False
     
+    # Write your logic here
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+    
+    return leap
+
