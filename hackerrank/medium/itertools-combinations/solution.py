e2a27dab4833dbb518dc3880d02639b85188a335
Radha Teli <radha952121@gmail.com>
2026-09-08 15:50:22 +0530

    ✅ itertools.combinations() | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-08

--- hackerrank/medium/itertools-combinations/solution.py
+++ hackerrank/medium/itertools-combinations/solution.py
@@ -1,16 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations
+
 S, k = input().split()
+S = sorted(S)
 k = int(k)
 
-S = sorted(S)
-
-def generate(current, remaining):
-    if len(current) == k:
-        print(''.join(current))
-        return
-
-    for i in range(len(remaining)):
-        generate(current + [remaining[i]], 
-                 remaining[:i] + remaining[i+1:])
-
-generate([], S)
+for i in range(1, k + 1):
+    for combination in combinations(S, i):
+        print(''.join(combination))
