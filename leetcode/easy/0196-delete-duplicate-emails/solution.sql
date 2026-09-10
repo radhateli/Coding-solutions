d88e0aec55c58bca84a9e182da5b487ad7054820
Radha Teli <radha952121@gmail.com>
2026-09-10 19:27:52 +0530

    ✅ Delete Duplicate Emails | Easy | SQL | Runtime: 698 ms (89.90%) | Memory: 0B | 2026-09-10

--- leetcode/easy/0196-delete-duplicate-emails/solution.sql
+++ leetcode/easy/0196-delete-duplicate-emails/solution.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
 delete from Person 
-where id not in (
-    select id 
-    from (
-        select min(id) AS id
-        from Person 
-        group by email
-        ) temp
-    )
+where id not in (select id 
+                 from (select min(id) AS id
+                       from Person 
+                       group by email
+                       ) temp);
