29a23354dc3d21c9fceebee192d0b182a71bafda
Radha Teli <radha952121@gmail.com>
2026-09-10 19:43:31 +0530

    ✅ GSQ05 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ codechef/medium/GSQ05/solution.sql
@@ -0,0 +1,4 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+INSERT INTO employee (employee_id, employee_name, department)
+VALUES (4, 'Marcus Garcia', 'Product'),
+       (5, 'Samantha Park', 'Hr');
