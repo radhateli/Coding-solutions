9d201060001120b652d4ccf1f1d5649fa3b67971
Radha Teli <radha952121@gmail.com>
2026-09-14 18:46:50 +0530

    ✅ GSQ10 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/GSQ10/solution.sql
+++ codechef/medium/GSQ10/solution.sql
@@ -1,7 +1,10 @@
-/*Write a query to set the Department as 'HR', for the employee with employee_id 2 to the existing table employee. */
-UPDATE employee 
-SET Department = 'Hr'
-WHERE employee_id = 2;
+/* Write a query which does the following
+- Add a new column 'Hourly_Pay' to the table employee and set the value as 100 by default.
+- Output the entire table
+*/
+
+ALTER TABLE employee
+ADD COLUMN Hourly_Pay  INT default 100;
 
 SELECT *
 FROM employee;
