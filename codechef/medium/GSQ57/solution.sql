e7836b135ba4f3f94511dba96c56a8a8db5c9f9e
Radha Teli <radha952121@gmail.com>
2026-09-14 19:33:32 +0530

    ✅ GSQ57 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/GSQ57/solution.sql
+++ codechef/medium/GSQ57/solution.sql
@@ -1,8 +1,18 @@
-/*Write a query which does the following
-- Delete the entire row of the customer with customer id 1.
-- Output the entire table once the above action is performed. */
+/* Debug this query */
 
-DELETE FROM customer 
-WHERE Id = 1;
+CREATE TABLE customer
+(
+  Id INT PRIMARY KEY,
+  Name VARCHAR(30),
+  Age INT,
+  Address VARCHAR(30),
+  email VARCHAR(30) UNIQUE
+);
+INSERT INTO customer(Id,Name,Age,Address,email)
+VALUES  (1, 'John Smith', 25,  '123 Main St','john@example.com'),
+        (2, 'Sarah Johnson', 30,'456 Broadway','sarah@example.com'),
+        (3, 'Michael Brown', 45,  '789 5th Ave','michael@example.com'),
+        (4, 'Jessica Davis', 28,  '321 Elm St','Jessica@example.com');
+        
+SELECT * FROM customer;
 
-SELECT * FROM customer; 
